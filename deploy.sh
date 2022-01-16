c29797acd602e84b63d37172b56d28924f2f3369
Shahrooz Aghili <shahrooz.aghili@main-incubator.com>
2022-01-16 14:21:11 +0100

    bugfix worker

--- deploy.sh
+++ deploy.sh
@@ -13,4 +13,4 @@ kubectl apply -f k8s
 
 kubectl set image deployments/server-deployment server=shahrooz33ce/multi-server:$SHA
 kubectl set image deployments/client-deployment client=shahrooz33ce/multi-client:$SHA
-kubectl set image deployments/worker-deployment wroker=shahrooz33ce/multi-worker:$SHA
+kubectl set image deployments/worker-deployment worker=shahrooz33ce/multi-worker:$SHA
